9998af8235cb8ccd80f375cbfca75cc9f87eaf82
MIJAEL888 <mijael888@gmail.com>
2017-08-30 12:37:49 -0500

    avance de los Apis

---
+++ ClinicaAPI/ClinicaEntity/HistoriaClinica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaEntity
+{
+    [Table("HistoriaClinica")]
+    public class HistoriaClinica
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Observacion { get; set; }
+
+        [ForeignKey("Diagnostico")]
+        public int IdDiagnostico { get; set; }
+        public Diagnostico Diagnostico { get; set; }
+
+        [ForeignKey("Tratamiento")]
+        public int IdTratamiento { get; set; }
+        public Tratamiento Tratamiento { get; set; }
+
+    }
+}
